0ac568b800cc50c58462ccc8db64a4a254bfbc13
velsin <16018965+velsin@users.noreply.github.com>
2025-04-13 18:16:30 +0200

    Add final sequences and benchmarking.

--- pkg/warmup/RecursiveSequences.go
+++ pkg/warmup/RecursiveSequences.go
@@ -41,12 +41,51 @@ func GenerateSequence(fn PureFuncIntInt, n int) []int {
 	return res
 }
 
-func sequence1(in int) int {
-	// Base cases
-	if in == 1 {
+func sequence1(n int) int {
+	if n == 1 {
 		return 5
 	}
-	return (sequence1(in-1) * 3) - 4
+	return (sequence1(n-1) * 3) - 4
 }
 
 var Sequence1 = Memoized(sequence1)
+
+func sequence2(n int) int {
+	if n == 1 {
+		return 25
+	}
+
+	prev := sequence2(n - 1)
+
+	if prev%2 == 0 {
+		return prev / 2
+	} else {
+		return (prev * 3) + 1
+	}
+}
+
+var Sequence2 = Memoized(sequence2)
+
+func sequence3(n int) int {
+	if n == 1 {
+		return 2
+	}
+	if n == 2 {
+		return -3
+	}
+	return sequence3(n-1) * sequence3(n-2)
+}
+
+var Sequence3 = Memoized(sequence3)
+
+func fibonacci(n int) int {
+	if n == 1 {
+		return 0
+	}
+	if n == 2 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+var Fibonacci = Memoized(fibonacci)
--- pkg/warmup/RecursiveSequences_test.go
+++ pkg/warmup/RecursiveSequences_test.go
@@ -22,9 +22,69 @@ func TestSequence1(t *testing.T) {
 	}
 }
 
+func TestSequence2(t *testing.T) {
+	testCases := []struct {
+		in  int
+		exp int
+	}{
+		{1, 25},
+		{2, 76},
+		{3, 38},
+		{4, 19},
+	}
+
+	for _, test_case := range testCases {
+		res := Sequence2(test_case.in)
+		if res != test_case.exp {
+			t.Errorf("Got %d for %d, expected %d", res, test_case.in, test_case.exp)
+		}
+	}
+}
+
+func TestSequence3(t *testing.T) {
+	testCases := []struct {
+		in  int
+		exp int
+	}{
+		{1, 2},
+		{2, -3},
+		{3, -6},
+		{4, 18},
+		{5, -108},
+	}
+
+	for _, test_case := range testCases {
+		res := Sequence3(test_case.in)
+		if res != test_case.exp {
+			t.Errorf("Got %d for %d, expected %d", res, test_case.in, test_case.exp)
+		}
+	}
+}
+
+func TestFibonacci(t *testing.T) {
+	testCases := []struct {
+		in  int
+		exp int
+	}{
+		{1, 0},
+		{2, 1},
+		{3, 1},
+		{4, 2},
+		{5, 3},
+		{6, 5},
+	}
+
+	for _, test_case := range testCases {
+		res := Fibonacci(test_case.in)
+		if res != test_case.exp {
+			t.Errorf("Got %d for %d, expected %d", res, test_case.in, test_case.exp)
+		}
+	}
+}
+
 // --- Benchmarks ---
 
-const benchmarkSequenceLength = 50
+const benchmarkSequenceLength = 30
 
 func BenchmarkSequence1Naive(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -38,4 +98,41 @@ func BenchmarkSequence1Memoized(b *testing.B) {
 	}
 	// It seems like this version might actually persist the same cache across
 	// all b.N benchmark runs, favoring the memoized version even more.
+}
+
+func BenchmarkSequence2Naive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = GenerateSequence(sequence2, benchmarkSequenceLength)
+	}
+}
+
+func BenchmarkSequence2Memoized(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = GenerateSequence(Sequence2, benchmarkSequenceLength)
+	}
+}
+
+func BenchmarkSequence3Naive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// Set much lower seq length since this one quickly explodes past int64 size?
+		_ = GenerateSequence(sequence3, benchmarkSequenceLength)
+	}
+}
+
+func BenchmarkSequence3Memoized(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = GenerateSequence(Sequence3, benchmarkSequenceLength)
+	}
+}
+
+func BenchmarkFibonacciNaive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = GenerateSequence(fibonacci, benchmarkSequenceLength)
+	}
+}
+
+func BenchmarkFibonacciMemoized(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = GenerateSequence(Fibonacci, benchmarkSequenceLength)
+	}
 }
